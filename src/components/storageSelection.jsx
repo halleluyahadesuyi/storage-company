458709fa11e0862d77a7d71cf9c9d243e075527b
Halleluyah Adesuyi <adesuyihalleluyah@gmail.com>
2024-07-16 23:38:02 +0100

    feat: Create storage selection section

--- src/components/storageSelection.jsx
+++ src/components/storageSelection.jsx
@@ -1,7 +1,20 @@
+import { FaArrowRight } from "react-icons/fa";
+
 export function storageSelection () {
     return (
         <>
-            
+            <h3>I'm looking for</h3>
+            <div className={style.storage}>
+                <div className={style.houseStorage}>
+                    <img src={houseStorage} alt="house storage" className="storage" />
+                    <div>House storage <FaArrowRight /></div>
+                </div>
+
+                <div className={style.businessStorage}>
+                    <img src={businessStorage} alt="business storage" className="storage" />
+                    <div>Business storage <FaArrowRight /></div>
+                </div>
+            </div>
         </>
     )
 }
