216f370bb4d56410d3a832d38dc8418adb27aae4
Halleluyah Adesuyi <adesuyihalleluyah@gmail.com>
2024-07-16 23:47:43 +0100

    feat: Add size guide area

--- src/components/storageSelection.jsx
+++ src/components/storageSelection.jsx
@@ -1,19 +1,27 @@
 import { FaArrowRight } from "react-icons/fa";
+import houseStorage from "../assets/imgs/city.png";
+import businessStorage from "../assets/imgs/city.png";
+import styles from "../css/Home.module.css";
 
 export function storageSelection () {
     return (
         <>
             <h3>I'm looking for</h3>
-            <div className={style.storage}>
-                <div className={style.houseStorage}>
-                    <img src={houseStorage} alt="house storage" className="storage" />
+            <div className={styles.storage}>
+                <div className={styles.houseStorage}>
+                    <img src={houseStorage} alt="house storage" />
                     <div>House storage <FaArrowRight /></div>
                 </div>
 
                 <div className={style.businessStorage}>
-                    <img src={businessStorage} alt="business storage" className="storage" />
+                    <img src={businessStorage} alt="business storage" />
                     <div>Business storage <FaArrowRight /></div>
                 </div>
+            </div>
+            <div className={styles.guide}>
+                <img src={ruler} alt="ruler" className={ruler} />
+                <div>Not sure what size you need?</div>
+                <button>Try our size guide <FaArrowRight /></button>
             </div>
         </>
     )
