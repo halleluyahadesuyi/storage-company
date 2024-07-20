d88a9eca2650eb23e65e2c55e2f7c71b387bd08d
Halleluyah Adesuyi <adesuyihalleluyah@gmail.com>
2024-07-20 15:55:41 +0100

    feat: Align get-a-quote with header

--- src/components/StorageSelection.jsx
+++ src/components/StorageSelection.jsx
@@ -7,32 +7,34 @@ import styles from "../css/StorageSelection.module.css";
 
 export function StorageSelection() {
   return (
-    <section className={styles.storageSelection}>
+    <>
       <hr className={styles.faintLine} />
-      <h2 className={styles.header}>I'm looking for</h2>
-      <div className={styles.storage}>
-        <div className={styles.houseStorage}>
-          <img src={houseStorage} alt="house storage" />
-          <div className={styles.storagelink}>
-            House storage <FaArrowRight className={styles.arrow} />
+      <section className={styles.storageSelection}>
+        <h2>I'm looking for</h2>
+        <div className={styles.storage}>
+          <div className={styles.houseStorage}>
+            <img src={houseStorage} alt="house storage" />
+            <div className={styles.storagelink}>
+              House storage <FaArrowRight className={styles.arrow} />
+            </div>
           </div>
-        </div>
 
-        <div className={styles.businessStorage}>
-          <img src={businessStorage} alt="business storage" />
-          <div className={styles.storageLink}>
-            Business storage <FaArrowRight className={styles.arrow} />
+          <div className={styles.businessStorage}>
+            <img src={businessStorage} alt="business storage" />
+            <div className={styles.storageLink}>
+              Business storage <FaArrowRight className={styles.arrow} />
+            </div>
           </div>
         </div>
-      </div>
-      <div className={styles.guide}>
-        <img src={ruler} alt="ruler" className={styles.ruler} />
-        <p>Not sure what size you need?</p>
-        <button>
-          Try our size guide <FaArrowRight className={styles.arrow} />
-        </button>
-        <img src={building} alt="building" className={styles.building} />
-      </div>
-    </section>
+        <div className={styles.guide}>
+          <img src={ruler} alt="ruler" className={styles.ruler} />
+          <p>Not sure what size you need?</p>
+          <button>
+            Try our size guide <FaArrowRight className={styles.arrow} />
+          </button>
+          <img src={building} alt="building" className={styles.building} />
+        </div>
+      </section>
+    </>
   );
 }
--- src/components/StoreWithUs.jsx
+++ src/components/StoreWithUs.jsx
@@ -11,75 +11,76 @@ import styles from "../css/StoreWithUs.module.css";
 
 export function StoreWithUs() {
   return (
-    <section className={styles.store}>
-      <h3>Why store with us?</h3>
-      <section className={styles.qualities}>
-        <h2>We're safe, easy & affordable</h2>
+    <>
+      <hr className={styles.faintLine} />
+      <section className={styles.store}>
+        <h3>Why store with us?</h3>
+        <section className={styles.qualities}>
+          <h2>We're safe, easy & affordable</h2>
+          <button className={styles.gradientButton}>
+            Get a quote
+            <FaArrowRight
+              style={{ padding: "0 2px 0 12px", transform: "scaleX(1.4)" }}
+            />
+          </button>
+        </section>
 
-        {/* Button to get a quote with a gradient background and an arrow icon */}
-        <button className={styles.gradientButton}>
-          Get a quote
-          <FaArrowRight
-            style={{ padding: "0 2px 0 12px", transform: "scaleX(1.4)" }}
-          />
-        </button>
+        <section className={styles.safe}>
+          <img src={safe} alt="safe" />
+
+          <div>
+            <h3>Safe</h3>
+            <img src={checkmark} alt="checkmark" /> 24 hour CCTV storage
+            <img src={checkmark} alt="checkmark" /> 10ft steel perimeter
+            ring-fencing
+            <img src={checkmark} alt="checkmark" /> Heavy duty industrial locks
+            <img src={checkmark} alt="checkmark" /> Electronic gates control
+            access
+          </div>
+        </section>
+
+        <section className={styles.easy}>
+          <img src={easy} alt="easy" />
+
+          <div>
+            <h3>Easy</h3>
+            <img src={checkmark} alt="checkmark" /> 24 hour access
+            <img src={checkmark} alt="checkmark" /> Ground floor access
+            <img src={checkmark} alt="checkmark" /> Range of sizes
+            <img src={checkmark} alt="checkmark" /> Same day availability
+          </div>
+        </section>
+
+        <section className={styles.affordable}>
+          <img src={affordable} alt="affordable" />
+
+          <div>
+            <h3>Affordable</h3>
+            <img src={checkmark} alt="checkmark" /> No large advertising spend
+            <img src={checkmark} alt="checkmark" /> No expensive head office
+            overheads
+            <img src={checkmark} alt="checkmark" /> No long-term contracts
+            <img src={checkmark} alt="checkmark" /> Regular price checks with
+            competitors
+          </div>
+        </section>
+
+        <section>
+          <img src={plusmark} alt="plus mark" />
+          <div>
+            <h3>Plus, we provide a high quality, personal service</h3>
+            <p>With over 50% of our new customers come from recommendations</p>
+          </div>
+          <div>
+            <img src={customerLead} alt="customer lead" />
+            Sarah, Customer Lead
+          </div>
+
+          <img src={stars} alt="stars" />
+          <p>4.96 average rating across 167 reviews</p>
+          <a href="/">See all reviews</a>
+        </section>
       </section>
-
-      <section className={styles.safe}>
-        <img src={safe} alt="safe" />
-
-        <div>
-          <h3>Safe</h3>
-          <img src={checkmark} alt="checkmark" /> 24 hour CCTV storage
-          <img src={checkmark} alt="checkmark" /> 10ft steel perimeter
-          ring-fencing
-          <img src={checkmark} alt="checkmark" /> Heavy duty industrial locks
-          <img src={checkmark} alt="checkmark" /> Electronic gates control
-          access
-        </div>
-      </section>
-
-      <section className={styles.easy}>
-        <img src={easy} alt="easy" />
-
-        <div>
-          <h3>Easy</h3>
-          <img src={checkmark} alt="checkmark" /> 24 hour access
-          <img src={checkmark} alt="checkmark" /> Ground floor access
-          <img src={checkmark} alt="checkmark" /> Range of sizes
-          <img src={checkmark} alt="checkmark" /> Same day availability
-        </div>
-      </section>
-
-      <section className={styles.affordable}>
-        <img src={affordable} alt="affordable" />
-
-        <div>
-          <h3>Affordable</h3>
-          <img src={checkmark} alt="checkmark" /> No large advertising spend
-          <img src={checkmark} alt="checkmark" /> No expensive head office
-          overheads
-          <img src={checkmark} alt="checkmark" /> No long-term contracts
-          <img src={checkmark} alt="checkmark" /> Regular price checks with
-          competitors
-        </div>
-      </section>
-
-      <section>
-        <img src={plusmark} alt="plus mark" />
-        <div>
-          <h3>Plus, we provide a high quality, personal service</h3>
-          <p>With over 50% of our new customers come from recommendations</p>
-        </div>
-        <div>
-          <img src={customerLead} alt="customer lead" />
-          Sarah, Customer Lead
-        </div>
-
-        <img src={stars} alt="stars" />
-        <p>4.96 average rating across 167 reviews</p>
-        <a href="/">See all reviews</a>
-      </section>
-    </section>
+    </>
   );
 }
