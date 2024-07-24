d60d057d2511eefce31c0ad9265bb89068ed4266
Halleluyah Adesuyi <adesuyihalleluyah@gmail.com>
2024-07-24 16:05:23 +0100

    feat: Incorporate store details for customers

--- src/components/StoreWithUs.jsx
+++ src/components/StoreWithUs.jsx
@@ -25,58 +25,90 @@ export function StoreWithUs() {
           </button>
         </section>
 
-        <section className={styles.safe}>
-          <img src={safe} alt="safe" />
+        <section className={styles.description}>
+          <section className={styles.safe}>
+            <img src={safe} alt="safe" />
+            <div>
+              <h3>Safe</h3>
+              <hr className={styles.faintLine} />
+              <ul>
+                <li>
+                  <img src={checkmark} alt="checkmark" /> 24 hour CCTV storage
+                </li>
+                <li>
+                  <img src={checkmark} alt="checkmark" /> 10ft steel perimeter
+                  ring-fencing
+                </li>
+                <li>
+                  <img src={checkmark} alt="checkmark" /> Heavy duty industrial
+                  locks
+                </li>
+                <li>
+                  <img src={checkmark} alt="checkmark" /> Electronic gates
+                  control access
+                </li>
+              </ul>
+            </div>
+          </section>
 
-          <div>
-            <h3>Safe</h3>
-            <img src={checkmark} alt="checkmark" /> 24 hour CCTV storage
-            <img src={checkmark} alt="checkmark" /> 10ft steel perimeter
-            ring-fencing
-            <img src={checkmark} alt="checkmark" /> Heavy duty industrial locks
-            <img src={checkmark} alt="checkmark" /> Electronic gates control
-            access
-          </div>
+          <section className={styles.easy}>
+            <img src={easy} alt="easy" />
+            <div>
+              <h3>Easy</h3>
+              <hr className={styles.faintLine} />
+              <ul>
+                <li>
+                  <img src={checkmark} alt="checkmark" /> 24 hour access
+                </li>
+                <li>
+                  <img src={checkmark} alt="checkmark" /> Ground floor access
+                </li>
+                <li>
+                  <img src={checkmark} alt="checkmark" /> Range of sizes
+                </li>
+                <li>
+                  <img src={checkmark} alt="checkmark" /> Same day availability
+                </li>
+              </ul>
+            </div>
+          </section>
+
+          <section className={styles.affordable}>
+            <img src={affordable} alt="affordable" />
+            <div>
+              <h3>Affordable</h3>
+              <hr className={styles.faintLine} />
+              <ul>
+                <li>
+                  <img src={checkmark} alt="checkmark" /> No large advertising spend
+                </li>
+                <li>
+                  <img src={checkmark} alt="checkmark" /> No expensive head office overheads
+                </li>
+                <li>
+                  <img src={checkmark} alt="checkmark" /> No long-term contracts
+                </li>
+                <li>
+                  <img src={checkmark} alt="checkmark" /> Regular price checks
+                  with competitors
+                </li>
+              </ul>
+            </div>
+          </section>
         </section>
 
-        <section className={styles.easy}>
-          <img src={easy} alt="easy" />
-
-          <div>
-            <h3>Easy</h3>
-            <img src={checkmark} alt="checkmark" /> 24 hour access
-            <img src={checkmark} alt="checkmark" /> Ground floor access
-            <img src={checkmark} alt="checkmark" /> Range of sizes
-            <img src={checkmark} alt="checkmark" /> Same day availability
-          </div>
-        </section>
-
-        <section className={styles.affordable}>
-          <img src={affordable} alt="affordable" />
-
-          <div>
-            <h3>Affordable</h3>
-            <img src={checkmark} alt="checkmark" /> No large advertising spend
-            <img src={checkmark} alt="checkmark" /> No expensive head office
-            overheads
-            <img src={checkmark} alt="checkmark" /> No long-term contracts
-            <img src={checkmark} alt="checkmark" /> Regular price checks with
-            competitors
-          </div>
-        </section>
-
-        <section>
-          <img src={plusmark} alt="plus mark" />
+        <section className={styles.recommendations}>
+          <img src={plusmark} alt="plus mark" className={styles.plusMark} />
           <div>
             <h3>Plus, we provide a high quality, personal service</h3>
             <p>With over 50% of our new customers come from recommendations</p>
           </div>
-          <div>
+          <div className={styles.customerLead}>
             <img src={customerLead} alt="customer lead" />
             Sarah, Customer Lead
           </div>
 
-          <img src={stars} alt="stars" />
+          <img src={stars} alt="stars" className={styles.stars} />
           <p>4.96 average rating across 167 reviews</p>
           <a href="/">See all reviews</a>
         </section>
